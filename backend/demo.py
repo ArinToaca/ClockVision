d04598c5184324d55bcb4a9695ba497fa4bd5333
Arin Toaca <arin.toaca@gmail.com>
2018-05-12 15:29:15 +0300

    Working backend.

--- backend/demo.py
+++ backend/demo.py
@@ -54,52 +54,98 @@ def get_db():
 # ROUTES
 
 
-@app.route('/set_worker', methods=['POST'])
+@app.route('/worker', methods=['POST'])
 def set_worker():
     db = get_db()
     if request.method == 'POST':
         db.execute('insert into workers (at_work, '
                    'name) values (?, ?)',
-                   [request.form['at_work'],
+                   [0,  # initially the new worker is not at work.
                     request.form['name']])
         db.commit()
         flash("New entry was successfully posted")
+        result = dict()
+        cursor = db.execute('select worker_id from workers order by '
+                            'worker_id desc limit 1')
+        result['worker_id'] = cursor.fetchone()[0]
+        return jsonify(result)
+    elif request.method == 'GET':
+        cursor = db.execute('select * from workers where '
+                            'worker_id == ?', request.args.get('worker_id'))
+        columns = [column[0] for column in cursor.description]
+        row = cursor.fetchone()
+        result = dict(zip(columns, row))
+        return jsonify(result)
+
     return 'OK'
 
 
-@app.route('/worker_history', methods=['POST'])
+@app.route('/worker_history', methods=['POST', 'GET'])
 def worker_history():
     db = get_db()
     if request.method == 'POST':
         cursor = db.execute('select at_work from workers'
                             'where worker_id == ?',
                             [request.args.get('worker_id')])
-        at_work = False
-        for row in cursor.fetchall():
-            at_work = bool(row)
+
+        start_work, end_work = 0, 0
+        at_work = not bool(cursor.fetchone()[0])
         if at_work:
-            # GET LATEST ENTRY BY HISTORY ID
-            # UPDATE
-            cursor = db.execute('update worker_history'
-                                'set start_work = ?, '
-                                'end_work = ?, '
-                                'hours_worked = ? '
-                                'where history_id in (select history_id from '
-                                'worker_history order by id desc limit 1 where'
-                                'worker_id == ?)',
-                                [request.form['start_work'],
-                                 request.form['end_work'],
-                                 request.form['hours_worked'],
-                                 request.args.get('worker_id')])
+            start_work = request.form['timestamp']
+            # INSERT NEW HISTORY
+            db.execute('insert into worker_history (start_work, '
+                       'end_work, hours_worked, worker_id) '
+                       'values (?, ?, ?,?)',
+                       [start_work,
+                        end_work,
+                        0,  # field only valid at end of workday.
+                        request.args.get('worker_id')])
+            db.execute('update workers'
+                       'set at_work = ?'
+                       'where worker_id == ?',
+                       [at_work,
+                        request.args.get('worker_id')])
         else:
-            # INSERT
-            cursor = db.execute('insert into worker_history (start_work, '
-                                'end_work, hours_worked, worker_id) '
-                                'values (?, ?, ?,?)',
-                                [request.form['start_work'],
-                                 request.form['end_work'],
-                                 request.form['hours_worked'],
-                                 request.args.get('worker_id')])
+            end_work = request.form['timestamp']
+            # UPDATE EXISTING HISTORY
+            # get last entry history_id associated with worker_id
+            cursor = db.execute('select history_id from worker_history where '
+                                'worker_id == ? order by history_id '
+                                'desc limit 1',
+                                [request.args.get('worker_id')])
+            history_id = cursor.fetchone()[0]
+            # for worker_history table
+            db.execute('update worker_history'
+                       'set start_work = ?, '
+                       'end_work = ?, '
+                       'hours_worked = ? '
+                       'where history_id == ?'
+                       [start_work,
+                        end_work,
+                        (end_work - start_work),  # simple diff atm
+                        history_id])
+            # for workers table
+            db.execute('update workers'
+                       'set at_work = ?'
+                       'where worker_id == ?',
+                       [at_work,
+                        request.args.get('worker_id')])
+        db.commit()
+        flash("Successfully added timestamp.")
+        return 'OK'
+
+    elif request.method == 'GET':
+        # get worker_history & if worker is at work
+        cursor = db.execute('select * from worker_history where '
+                            'worker_id == ?', request.args.get('worker_id'))
+        columns = [column[0] for column in cursor.description]
+        results = []
+        for row in cursor.fetchall():
+            results.append(dict(zip(columns, row)))
+        cursor = db.execute('select at_work from workers where worker_id == ?',
+                            request.args.get('worker_id'))
+        return jsonify(results)
+
     return 'OK'
 
 
